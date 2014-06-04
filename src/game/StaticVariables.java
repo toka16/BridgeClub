c4b264bd8a907f014088b4f27ea1b237ae736e95
toka16 <tabra12@freeuni.edu.ge>
2014-06-04 14:39:58 +0400

    test change2

--- src/game/StaticVariables.java
+++ src/game/StaticVariables.java
@@ -3,7 +3,7 @@ package game;
 public class StaticVariables {
 	public static enum Sides {NORTH, EAST, SOUTH, WEST};
 	public static enum Suits {CLUB, DIAMOND, HEART, SPADE, NOTRUMP};
-	public static enum Nominals {e0, e1, DEUCE, TRIPLE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}; 
+	public static enum Nominals {e0, e1, DEUCE, TRIPLE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEENa, KING, ACE}; 
 	public static enum Vulnerables {NOLINE, NS, EW, BOTHLINES};
 }
 
